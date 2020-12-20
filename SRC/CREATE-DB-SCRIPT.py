a7d384b72481800745f7c27ce2805dd5da02cef7
sahar gavriely <gav6sa@gmail.com>
2020-12-20 22:51:14 +0200

    api bs

---
+++ SRC/CREATE-DB-SCRIPT.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from mysql.connector import errorcode
+from datetime import date
+from datetime import datetime
+import queries
+
+'''
+    details = {
+        'user': 'DbMysql02',
+        'password': 'lennon',
+        'host': '127.0.0.1',
+        'database': 'DbMysql0',
+        'port': 86,
+        'raise_on_warnings': True,
+    }
+'''
